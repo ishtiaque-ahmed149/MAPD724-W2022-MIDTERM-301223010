8f0cce75d027cc4f7b3bb214e99b7ac86f9c651a
Ishtiaque Ahmed <iahmed85@my.centennialcollege.ca>
2022-03-04 14:33:50 -0500

    added comments, fixed dock51 font issues

--- Scenes/GameScene.swift
+++ Scenes/GameScene.swift
@@ -1,3 +1,10 @@
+//GameScene.swift
+//Name: Ishtiaque Ahmed
+//StudentID: 301223010
+//Date: March 4th, 2022
+//App Description: This is a mail pilot app that was initially working only as portrait mode on simulator. According to the midterm test instruction, the app needed to be adjusted to work in landscape mode. All the components appear from right side of the screen. The plane is on the left side of the landscape screen that scrolls top-bottom. The islands appear from the right side of the screen to the left. The ocean scrolls from screen's right to left, now there are 2 clouds with horizontal speed and vertical drift.
+//Version: Mail Pilot 1.1
+
 import UIKit
 import AVFoundation
 import SpriteKit
