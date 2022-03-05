146220cc1c4cb0efb950cc517520b0e5733f81be
Ishtiaque Ahmed <iahmed85@my.centennialcollege.ca>
2022-03-05 18:47:14 -0500

    final commit

--- Scenes/EndScene.swift
+++ Scenes/EndScene.swift
@@ -2,7 +2,7 @@
 //Name: Ishtiaque Ahmed
 //StudentID: 301223010
 //Date: March 4th, 2022
-//App Description: This is a mail pilot app that was initially working only as portrait mode on simulator. According to the midterm test instruction, the app needed to be adjusted to work in landscape mode. All the components appear from right side of the screen. The plane is on the left side of the landscape screen that scrolls top-bottom. The islands appear from the right side of the screen to the left. The ocean scrolls from screen's right to left, now there are 2 clouds with horizontal speed and vertical drift.
+//App Description: This is a mail pilot app that was initially working only as portrait mode on simulator. According to the midterm test instruction, the app needed to be adjusted to work in landscape mode. All the components appear from right side of the screen. The plane is on the left side of the landscape screen that scrolls top-bottom. The islands appear from the right side of the screen to the left. The ocean scrolls from screen's right to left, now there are 2 clouds with horizontal speed and vertical drift. If the plane touches an island, player gets 100 score; if the plane touches a cloud player loses 1 life.
 //Version: Mail Pilot 1.1
 
 import SpriteKit
